e0b5e3204f03cb7af0a83bf20f6986b1a6bf315b
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2023-12-14 14:13:17 +0000

    Changes added

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -2,7 +2,9 @@ from lib.user import User
 
 class UserRepository:
     def __init__(self, connection):
-        self.connection = connection
+        self._connection = connection
+        self.existing_user = False
+        self.existing_email = False
     
     def all(self):
         rows = self._connection.execute('SELECT * from users')
@@ -11,4 +13,53 @@ class UserRepository:
             user = User(row['id'], row['username'], row['password'], row['email'])
             users.append(user)
         return users
+    
         
+    def find(self, user_id):
+        rows = self._connection.execute(
+            'SELECT * from users WHERE id = %s', [user_id])
+        
+        if rows:
+            row = rows[0]
+            return User(row['id'], row['username'], row['password'], row['email'])
+        else:
+            return None
+    
+    def find_by_name(self, username):
+        rows = self._connection.execute(
+            'SELECT * from users WHERE username = %s', [username])
+         
+        if rows:
+            row = rows[0]
+            return User(row['id'], row['username'], row['password'], row['email'])
+        else:
+            return None
+    
+    def find_by_email(self, email):
+        rows = self._connection.execute(
+            'SELECT * from users WHERE email = %s', [email])
+         
+        if rows:
+            row = rows[0]
+            return User(row['id'], row['username'], row['password'], row['email'])
+        else:
+            return None
+    
+    
+    def create(self, user):
+        user_already_exist = self.find_by_name(user.username)
+        email_already_exist = self.find_by_email(user.email)
+        if not user_already_exist or not email_already_exist:
+            rows = self._connection.execute('INSERT INTO users (username, password, email) VALUES (%s, %s, %s) RETURNING id', [
+                                            user.username, user.password, user.email])
+            
+            row = rows[0]
+            user.id = self._generate_next_id()
+            user.id = row["id"]
+            return True, user 
+        else:
+            return False, None 
+        
+    def _generate_next_id(self):
+        users = self.all()
+        return max([user.id for user in users], default=0) + 1
