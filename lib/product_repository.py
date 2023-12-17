5df4423370a56f1366be8d4fa8b2ea1fcf439c2c
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2023-12-17 13:40:53 +0000

    comments added.

--- lib/product_repository.py
+++ lib/product_repository.py
@@ -1,26 +1,35 @@
+# Import the Product class from the "lib.product" module
 from lib.product import Product
 
+# Define a class representing a repository for managing products
 class ProductRepository:
+    # Constructor method that initializes the repository with a database connection
     def __init__(self, connection):
         self._connection = connection
     
+    # Retrieve all products from the database and return them as a list of Product objects
     def all(self):
         rows = self._connection.execute('SELECT * from products')
         products = []
         for row in rows:
+            # Create a Product object for each row and append it to the list
             product = Product(row['id'], row['product_name'], row['quantity'], row['category'], row['price'], row['user_id'])
             products.append(product)
         return products
     
+    # Calculate and return aggregated information about the products for a specific user
     def product_count(self, current_user):
         rows = self._connection.execute('SELECT * from products')
         products = []
         for row in rows:
+            # Create a Product object for each row and append it to the list
             product = Product(row['id'], row['product_name'], row['quantity'], row['category'], row['price'], row['user_id'])
             products.append(product)
         
+        # Initialize a dictionary to store aggregated information
         result = {}
         
+        # Iterate through each product and aggregate information based on user and category
         for product in products:
             if current_user == product.user_id:
                 category = product.category
@@ -28,7 +37,7 @@ class ProductRepository:
                 user_id = product.user_id
                 total_quantity = product.quantity
         
-                
+                # Update the result dictionary with aggregated information
                 if category in result:
                     result[category]['quantity'] += int(total_quantity)
                     result[category]['category_count'] += 1
@@ -37,40 +46,39 @@ class ProductRepository:
                 else:
                     result[category] = {'category_count': 1, 'price': price, 'user_id': user_id, 'quantity': total_quantity}
       
-   
+        # Return the aggregated information
         return result
         
-        
+    # Find and return a product based on its ID
     def find(self, product_id):
-        rows = self._connection.execute(
-            'SELECT * from products WHERE id = %s', [product_id])
+        rows = self._connection.execute('SELECT * from products WHERE id = %s', [product_id])
         
+        # Check if rows are not empty and create a Product object
         if rows:
             row = rows[0]
             return Product(row['id'], row['product_name'], row['quantity'], row['category'], row['price'], row['user_id'])
         else:
+            # Return None if the product is not found
             return None
 
-    
+    # Insert a new product into the database and return the updated Product object with the assigned ID
     def create(self, product):
         rows = self._connection.execute('INSERT INTO products (product_name, quantity, category, price, user_id) VALUES (%s, %s, %s, %s, %s) RETURNING id', [
                                         product.product_name, product.quantity, product.category, product.price, product.user_id])
         
+        # Get the assigned ID for the newly created product
         row = rows[0]
         product.id = self._generate_next_id()
         product.id = row["id"]
         return product 
 
-    
+    # Delete a product from the database based on its ID
     def delete(self, id):
-        self._connection.execute(
-            "DELETE FROM products WHERE id = %s",
-            [id]
-        )
-        return None
+        self._connection.execute("DELETE FROM products WHERE id = %s", [id])
+        # Return None after deleting the product
     
-    
-    # ensure that the nex id always the +1 from the latest created.     
+    # Ensure that the next ID is always one greater than the latest created product's ID     
     def _generate_next_id(self):
+        # Retrieve all products and calculate the next available ID
         users = self.all()
         return max([user.id for user in users], default=0) + 1
--- lib/user_repository.py
+++ lib/user_repository.py
@@ -1,88 +1,95 @@
+# Import the User class from the "lib.user" module
 from lib.user import User
 
+# Define a class representing a repository for managing user information
 class UserRepository:
+    # Constructor method that initializes the repository with a database connection
     def __init__(self, connection):
         self._connection = connection
+        # Flags to track the existence of a user or email during operations
         self.existing_user = False
         self.existing_email = False
     
+    # Retrieve all users from the database and return them as a list of User objects
     def all(self):
         rows = self._connection.execute('SELECT * from users')
         users = []
         for row in rows:
+            # Create a User object for each row and append it to the list
             user = User(row['id'], row['username'], row['password'], row['email'])
             users.append(user)
         return users
     
-        
+    # Find and return a user based on their ID
     def find(self, user_id):
-        rows = self._connection.execute(
-            'SELECT * from users WHERE id = %s', [user_id])
+        rows = self._connection.execute('SELECT * from users WHERE id = %s', [user_id])
         
+        # Check if rows are not empty and create a User object
         if rows:
             row = rows[0]
             return User(row['id'], row['username'], row['password'], row['email'])
         else:
+            # Return None if the user is not found
             return None
     
+    # Find and return a user based on their username
     def find_by_name(self, username):
-        rows = self._connection.execute(
-            'SELECT * from users WHERE username = %s', [username])
+        rows = self._connection.execute('SELECT * from users WHERE username = %s', [username])
          
+        # Check if rows are not empty and create a User object
         if rows:
             row = rows[0]
             return User(row['id'], row['username'], row['password'], row['email'])
         else:
+            # Return None if the user is not found
             return None
     
+    # Find and return a user based on their email
     def find_by_email(self, email):
-        rows = self._connection.execute(
-            'SELECT * from users WHERE email = %s', [email])
+        rows = self._connection.execute('SELECT * from users WHERE email = %s', [email])
          
+        # Check if rows are not empty and create a User object
         if rows:
             row = rows[0]
             return User(row['id'], row['username'], row['password'], row['email'])
         else:
+            # Return None if the user is not found
             return None
-        
+    
+    # Check if a username exists in the database
     def find_name(self, name):
-        rows = self._connection.execute(
-            'SELECT username FROM users WHERE username = %s', [name]
-        )
+        rows = self._connection.execute('SELECT username FROM users WHERE username = %s', [name])
         
+        # Check if rows are not empty and return the username if found, otherwise return None
         if rows:
             row = rows[0]
             return row['username'] if row else None
         else:
             return None
     
+    # Check if an email exists in the database
     def find_email(self, email):
-        rows = self._connection.execute(
-            'SELECT email FROM users WHERE email = %s', [email]
-        )
+        rows = self._connection.execute('SELECT email FROM users WHERE email = %s', [email])
         
+        # Check if rows are not empty and return the email if found, otherwise return None
         if rows:
             row = rows[0]
             return row['email'] if row else None
         else:
             return None
-        
     
+    # Create a new user in the database, checking for existing username and email
     def create(self, user):
+        # Check if the username and email already exist
         user_already_exist = self.find_name(user.username)
         email_already_exist = self.find_by_email(user.email)     
-        if user_already_exist == None and email_already_exist == None:
+        if user_already_exist is None and email_already_exist is None:
+            # Insert the new user into the database and return success along with the user information
             users = self._connection.execute('INSERT INTO users (username, password, email) VALUES (%s, %s, %s) RETURNING id, username, email', [
                                             user.username, user.password, user.email])
             
             user = users[0]
             return True, user
         else:
+            # Return failure if the username or email already exists
             return False, None
-      
-      
-      
-      
-
-    
-    
